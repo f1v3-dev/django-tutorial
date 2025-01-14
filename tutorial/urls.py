f26cd8dd7c25c8e67f136a74356f36d587224465
seungjo <f1v3@kakao.com>
2025-01-14 16:31:17 +0900

    feat: Serialization

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,13 +1,6 @@
-from django.urls import include, path
-from rest_framework import routers
-
-from quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+from django.urls import path, include
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
+    path('', include('snippets.urls'))
 ]
